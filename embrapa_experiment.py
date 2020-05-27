correcting avg val loss plots to take average based on folds, not epochs

embrapa_experiment.py
@@ -101,7 +101,7 @@ def plot_average_validation_loss(losses, number_of_epochs, number_of_folds, file
     for fold in range(number_of_folds):
         average_validation_loss += np.array(losses[f'fold#{fold}']["validation_loss"])
 
-    average_validation_loss /= number_of_epochs
+    average_validation_loss /= number_of_folds
 
     plt.figure()
     plt.plot(x, average_validation_loss)

plot_avg_val_loss_for_all_experiments.py
@@ -44,7 +44,7 @@ if __name__ == '__main__':
                 for fold in range(NUMBER_OF_FOLDS):
                     avg_val_losses_by_experiment[experiment_name] += np.array(losses_object[f'fold#{fold}']["validation_loss"])
 
-                avg_val_losses_by_experiment[experiment_name] /= number_of_epochs
+                avg_val_losses_by_experiment[experiment_name] /= NUMBER_OF_FOLDS
                 plt.plot(x, avg_val_losses_by_experiment[experiment_name], label=f'#{EXPERIMENT_NUMBER_BY_NAME[experiment_name]}')
 
     plt.legend()